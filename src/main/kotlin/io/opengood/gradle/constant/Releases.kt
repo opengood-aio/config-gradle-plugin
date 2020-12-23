920fc4475ddd37504c48baa585ae9b08bd339c1f
Chris Jaehnen <cjaehnen@me.com>
2020-12-23 16:41:06 -0500

    Update release plugin version pattern to increment minor version in config plugin

--- src/main/kotlin/io/opengood/gradle/constant/Releases.kt
+++ src/main/kotlin/io/opengood/gradle/constant/Releases.kt
@@ -1,5 +1,9 @@
 package io.opengood.gradle.constant
 
+import io.opengood.gradle.closure.KotlinClosure2
+import org.gradle.api.Project
+import java.util.regex.Matcher
+
 class Releases {
 
     companion object {
@@ -7,5 +11,11 @@ class Releases {
         const val PRE_TAG_COMMIT_MESSAGE = "[Gradle Release] - pre tag commit: "
         const val PUSH_TO_REMOTE = "origin"
         const val REQUIRE_BRANCH = ""
+
+        val VERSION_PATTERNS = mapOf(
+            """[.]*\.(\d+)\.(\d+)[.]*""" to KotlinClosure2<Matcher, Project, String>({ matcher, project ->
+                matcher.replaceAll(".${(matcher.group(0)[1]) + 1}.0")
+            })
+        )
     }
 }
