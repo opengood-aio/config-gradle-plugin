97036b3943f6860342ba703ca9c791699b646fee
Chris Jaehnen <cjaehnen@me.com>
2020-12-25 14:38:34 -0500

    Update Spring Boot and Kotest versions

--- src/main/kotlin/io/opengood/gradle/constant/Versions.kt
+++ src/main/kotlin/io/opengood/gradle/constant/Versions.kt
@@ -4,14 +4,14 @@ class Versions {
 
     companion object {
         const val ASSERTJ = "3.18.0"
-        const val GROOVY = "3.0.6"
+        const val GROOVY = "3.0.7"
         const val JAVA = "11"
         const val JUNIT_JUPITER = "5.7.0"
-        const val KOTEST = "4.3.1"
+        const val KOTEST = "4.3.2"
         const val KOTLIN = "1.4.21"
         const val LOMBOK = "1.18.16"
         const val MOCKITO = "3.6.28"
         const val MOCKK = "1.10.3"
-        const val SPRING_BOOT = "2.3.6.RELEASE"
+        const val SPRING_BOOT = "2.4.1"
     }
 }
