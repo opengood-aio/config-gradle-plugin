19204ce867106b7654e63d70a54b0569ce547344
Chris Jaehnen <cjaehnen@me.com>
2021-08-22 20:39:40 -0400

    Fix GitHub user environment variable

--- src/main/kotlin/io/opengood/gradle/constant/EnvVars.kt
+++ src/main/kotlin/io/opengood/gradle/constant/EnvVars.kt
@@ -4,7 +4,7 @@ class EnvVars {
 
     companion object {
         const val GITHUB_TOKEN = "GITHUB_TOKEN"
-        const val GITHUB_USER = "GITHUB_ACTOR"
+        const val GITHUB_USER = "GITHUB_USER"
         const val GPG_SIGNING_PASSWORD = "GPG_SIGNING_PASSWORD"
         const val GPG_SIGNING_PRIVATE_KEY = "GPG_SIGNING_PRIVATE_KEY"
         const val OSS_REPO_PASSWORD = "OSS_REPO_PASSWORD"
