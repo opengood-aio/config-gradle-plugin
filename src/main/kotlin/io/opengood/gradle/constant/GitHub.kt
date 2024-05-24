6e96f6382c705025e3bb312f355a074d2a75d903
Chris Jaehnen <cjaehnen@me.com>
2024-05-23 20:28:06 -0400

    Update to Spring Boot 3.3.0 and other dependencies

--- src/main/kotlin/io/opengood/gradle/constant/GitHub.kt
+++ src/main/kotlin/io/opengood/gradle/constant/GitHub.kt
@@ -4,7 +4,7 @@ class GitHub {
     companion object {
         const val BLOB_ENDPOINT_NAME = "blob"
         const val DEFAULT_BRANCH_NAME = "main"
-        const val OPENGOOD_ORG_NAME = "opengoodio"
+        const val OPENGOOD_ORG_NAME = "opengood-aio"
         const val OPENGOOD_ORG_URI = "https://github.com/$OPENGOOD_ORG_NAME"
         const val OPENGOOD_REPO_URI = "$OPENGOOD_ORG_URI/%s"
     }
